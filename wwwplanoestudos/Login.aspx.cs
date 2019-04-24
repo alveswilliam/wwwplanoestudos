760996d1300a9fd8f39f66fc244d132adc9b31ac
William Alves <ti.willam@faj.br>
2019-04-24 13:00:21 -0300

    Teste

--- wwwplanoestudos/Login.aspx.cs
+++ wwwplanoestudos/Login.aspx.cs
@@ -11,7 +11,7 @@ namespace wwwplanoestudos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("teste");
+            Response.Write("teste 2");
         }
     }
 }
