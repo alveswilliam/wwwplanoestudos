6f46b0811f458fc62ebb0d250db5a1107af275ec
William Alves <ti.willam@faj.br>
2019-05-03 08:19:47 -0300

    Criação da classe Plano

---
+++ wwwplanoestudos/_class/Plano.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wwwplanoestudos._class
+{
+    public class Plano
+    {
+        public Plano()
+        {
+
+        }
+
+        public string RA { get; set; }
+        public string CodCurso { get; set; }
+        public string CodPerlet { get; set; }
+        public int IdPerlet { get; set; }
+        public int CodColigada { get; set; }
+        public int CodTipoCurso { get; set; }
+        public int CodStatusPlano { get; set; }
+        public int CodStatusPlanoPago { get; set; }
+        public int CodStatusConfirmacao { get; set; }
+        public string Usuario { get; set; }
+        public string Status { get; set; }
+    }
+}
